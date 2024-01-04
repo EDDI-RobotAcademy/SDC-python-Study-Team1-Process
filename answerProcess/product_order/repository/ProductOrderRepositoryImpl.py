12f95850cdcddca158291813b22bf190bb4d897c
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2024-01-04 14:19:25 +0900

    feat:[조승훈]주문취소 repository에서 생성[PTANP-T1-25]

--- answerProcess/product_order/repository/ProductOrderRepositoryImpl.py
+++ answerProcess/product_order/repository/ProductOrderRepositoryImpl.py
@@ -17,8 +17,6 @@ class ProductOrderRepositoryImpl(ProductOrderRepository):
 
     def __init__(self):
         print("ProductOrderRepository 생성자 호출")
-        self.__receiverTask = None
-        self.__transmitterTask = None
 
     @classmethod
     def getInstance(cls):
@@ -42,12 +40,6 @@ class ProductOrderRepositoryImpl(ProductOrderRepository):
             print(f"DB 저장 중 에러 발생: {exception}")
             return None
 
-    def findAccountId(self, accountId):
-        dbSession = sessionmaker(bind=self.__instance.engine)
-        session = dbSession()
-
-        return session.query(ProductOrder).filter_by(_ProductOrder__accountId=accountId).first()
-
     def findAllProductIdByAccountId(self, accountId):
         dbSession = sessionmaker(bind=self.__instance.engine)
         session = dbSession()
@@ -58,3 +50,22 @@ class ProductOrderRepositoryImpl(ProductOrderRepository):
             productIdList.append(id.productNumber())
 
         return productIdList
+
+    def removeProductsByAccountId(self, sessionId, productId):
+        dbSession = sessionmaker(bind=self.__instance.engine)
+        session = dbSession()
+
+        products = session.query(ProductOrder).filter_by(_ProductOrder__accountId=sessionId,
+                                                         _ProductOrder__productId=productId).all()
+
+        if products:
+            for product in products:
+                session.delete(product)
+                session.commit()
+            response = True
+            print("주문 취소 완료")
+        else:
+            response = False
+            print("주문을 취소할 수 없습니다.")
+
+        return response
