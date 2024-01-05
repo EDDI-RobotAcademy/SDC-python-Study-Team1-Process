0e91e333379ccac4e773814c102dff7ba37f693c
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2024-01-05 10:58:05 +0900

    fix:[조승훈]사용자가 주문 조회를 요청하면 본인이 주문한 내역을 전달합니다.[PTANP-T1-24]

--- answerProcess/tests/product_order/service/TestProductOrderService.py
+++ answerProcess/tests/product_order/service/TestProductOrderService.py
@@ -26,7 +26,7 @@ class TestProductOrderService(unittest.TestCase):
         requestGeneratorService = RequestGeneratorServiceImpl.getInstance()
 
         productPurchaseRequestData = {
-            '__accountSessionId': 23,
+            '__accountSessionId': 22,
             '__productNumber': 7
         }
         protocolNumber = 9
@@ -44,7 +44,32 @@ class TestProductOrderService(unittest.TestCase):
         responseForm = responseGenerator(result)
         print(f"responseForm: {responseForm}")
 
+    def testProductOrderList(self):
+        initAccountDomain()
+        initProductDomain()
+        initOrderDomain()
 
+        initCustomProtocol()
+        testInstance = CustomProtocolRepositoryImpl.getInstance()
+        requestGeneratorService = RequestGeneratorServiceImpl.getInstance()
+
+        productPurchaseRequestData = {
+            '__accountSessionId': 22
+        }
+        protocolNumber = 11
+
+        requestGenerator = requestGeneratorService.findRequestGenerator(protocolNumber)
+        requestForm = requestGenerator(productPurchaseRequestData)
+        print(f"type(requestForm) = {type(requestForm)}")
+        result = testInstance.execute(protocolNumber, tuple(requestForm.__dict__.values()))
+        print(result)
+        print(f"type(result): {type(result)}")
+        responseGeneratorService = ResponseGeneratorServiceImpl.getInstance()
+
+        responseGenerator = responseGeneratorService.findResponseGenerator(protocolNumber)
+        print(result)
+        responseForm = responseGenerator(result)
+        print(f"responseForm: {responseForm}")
 
     if __name__ == '__main__':
         unittest.main()
